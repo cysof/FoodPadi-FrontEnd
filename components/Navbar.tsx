917e7a9e66895928de0c1f1b3fb5c20725edb0fe
ignatius omeje <ignatiusomeje153@gmail.com>
2025-06-14 10:50:37 +0100

    Fix: make the navbar to not touch the edges of the screen.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <div
-      className={`bg-white drop-shadow-sm border border-gray-300 rounded-full max-w-7xl w-full px-5 sm:px-7 py-3 .sm:py-5 fixed top-5 z-[999] right-1/2 translate-x-1/2  sm:mx-auto text-black`}
+      className={`  w-full px-2  fixed right-1/2 translate-x-1/2 top-5 z-[999] `}
     >
+      <div className={`bg-white drop-shadow-sm border border-gray-300 rounded-full max-w-7xl px-5 sm:px-7 py-3 .sm:py-5   sm:mx-auto text-black`}>
+
       {/* For Web */}
       <div
         className={`flex justify-between h-full gap-3 items-center w-full`}
@@ -26,6 +28,7 @@ const Navbar = () => {
         >
           Join Waitlist
         </Link>
+      </div>
       </div>
     </div>
   );
