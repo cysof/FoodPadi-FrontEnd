6f4d4a17df179c92f6380ab74c75fa326af2dc31
cysof <cp4reallife@yahoo.com>
2026-06-09 06:16:23 +0100

    update: Remove wait list from header component

--- features/home/components/Header.tsx
+++ features/home/components/Header.tsx
@@ -1,12 +1,10 @@
 "use client";
-
-import Link from "next/link";
+//import Link from "next/link";
 import { Carousel } from "primereact/carousel";
 import React from "react";
 
 const Header = () => {
   const imgArray = ["/HeaderBg.png", "/HeaderBg2.png"];
-
   const HeaderTemplate = (val: string) => (
     <div
       style={{
@@ -17,12 +15,8 @@ const Header = () => {
       }}
       className={`bg-no-repeat bg-center bg-cover h-dvh w-full`}
     >
-      <div
-        className={`h-full w-full flex justify-center items-center bg-black/50`}
-      >
-        <div
-          className={`max-w-7xl px-3 w-full mx-auto flex items-center flex-col gap-4`}
-        >
+      <div className={`h-full w-full flex justify-center items-center bg-black/50`}>
+        <div className={`max-w-7xl px-3 w-full mx-auto flex items-center flex-col gap-4`}>
           <h1
             data-aos="fade-up"
             data-aos-delay="200"
@@ -35,13 +29,6 @@ const Header = () => {
             Join the movement to simplify agriculture trading and delivery
             across Nigeria.
           </p>
-          <Link
-            className={`bg-primary mt-5 hover:bg-primary/90 text-white text-2xl px-7 py-3 rounded-2xl font-square`}
-            target="_blank"
-            href={`https://chat.whatsapp.com/CuueYeE9cQnJkgYgSLUDHC`}
-          >
-            Join the Waitlist
-          </Link>
         </div>
       </div>
     </div>
