973414a297f24f3c1e5245787ec5c4c822cb44cf
cysof <cp4reallife@yahoo.com>
2026-06-12 12:18:24 +0100

    authwrapper scroll bar added

--- components/AuthWrapper.tsx
+++ components/AuthWrapper.tsx
@@ -5,10 +5,7 @@ const AuthWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <div className={`bg-secondary flex w-full h-dvh overflow-hidden`}>
       <AuthSideBar />
-
-      <div
-        className={`overflow-y-auto noScroll w-full px-3 py-10 flex justify-center `}
-      >
+      <div className={`overflow-y-auto noScroll w-full px-3 py-10 flex items-start justify-center`}>
         {children}
       </div>
     </div>
